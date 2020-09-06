e340b3baf35d5922e5ff80290c4c28eb1c18f889
Amitadk22 <54771656+Amitadk22@users.noreply.github.com>
2020-09-06 23:14:52 +0530

    Create firstquestion.py

---
+++ Day3/firstquestion.py
@@ -0,0 +1,10 @@
+alt=int(input("Enter the altitude of the plane : "))
+
+if alt<=1000:
+    print("Land the plane safely")
+
+elif alt>1000 and alt<=5000:
+    print("Bring down to 1000")
+
+else :
+    print("Turn Around")
