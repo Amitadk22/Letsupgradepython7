baedec46c1290eada402b870287322228be68140
Amitadk22 <54771656+Amitadk22@users.noreply.github.com>
2020-09-06 23:11:44 +0530

    Create stringdatatype.py

---
+++ Day2/stringdatatype.py
@@ -0,0 +1,31 @@
+#strings
+
+a="hello"
+print(a)
+print(a[4])
+print(a[1:3])
+print(a.upper())
+print(a.capitalize())
+
+b='''hii,
+how are you?'''
+print(b)
+print(b[2:7])
+
+z="are" in b
+print("yes it is")
+
+c='  letsupgrade  '
+print(c)
+print(c.strip())
+
+d="hii,how are you?"
+print(d.split(","))
+print(d.count("h"),'times')
+print(d.find("e"),"position")
+
+x=a+d
+print(x)
+
+print(a.center(80))
+
